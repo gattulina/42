6d9c529dcf5b614788e11ebeceda99df570ff198
Luciana Gattuso <lugattus@c3r8s6.42malaga.com>
2024-08-15 18:53:06 +0200

    add

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: lugattus <lugattus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 18:51:28 by lugattus          #+#    #+#             */
-/*   Updated: 2024/05/24 16:51:30 by lugattus         ###   ########.fr       */
+/*   Updated: 2024/05/24 17:01:17 by lugattus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	char	*str;
 
 	i = 0;
-	str = (char	*)s;
+	str = (char *)s;
 	while (i < n)
 	{
 		if (str[i] == (char) c)
 			return (&str[i]);
 		i++
+		}
+			return (NULL);
 	}
-	return (NULL);
-}
